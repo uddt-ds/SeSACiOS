f2b72dd4a4d8da708673b8e0ec573b7045a6495a
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-19 19:51:07 +0900

    [Feat] BaseViewController 구현

---
+++ SimpleTableViewExample/SimpleTableViewExampleViewController/Base/BaseViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BaseViewController.swift
+//  SimpleTableViewExampleViewController
+//
+//  Created by Lee on 8/19/25.
+//
+
+import UIKit
+import RxSwift
+import SnapKit
+
+class BaseViewController: UIViewController {
+
+    var disposeBag = DisposeBag()
+    private(set) var didSetupConstraints = false
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        view.setNeedsUpdateConstraints()
+        configureHierarchy()
+    }
+
+    override func updateViewConstraints() {
+        if !self.didSetupConstraints {
+            configureConstraints()
+            didSetupConstraints = true
+        } else {
+            super.updateViewConstraints()
+        }
+    }
+
+    func configureConstraints() {
+
+    }
+
+    func configureHierarchy() {
+
+    }
+
+}
