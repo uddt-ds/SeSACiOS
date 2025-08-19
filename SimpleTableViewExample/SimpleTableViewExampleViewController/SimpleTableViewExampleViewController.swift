cee2b83466cf99ae0378e5bf855defc9d6afa33c
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-19 19:44:11 +0900

    [Chore] Initial commit

---
+++ SimpleTableViewExample/SimpleTableViewExampleViewController/SimpleTableViewExampleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTableViewExampleViewController
+//
+//  Created by Lee on 8/19/25.
+//
+
+import UIKit
+
+final class SimpleTableViewExampleViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+
+
+}
+
