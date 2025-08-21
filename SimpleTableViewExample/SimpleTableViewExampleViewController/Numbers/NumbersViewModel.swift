3d08a45ecc82da3978a7c1dd005abc228e80052d
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-21 17:06:17 +0900

    [Feat] NumbersViewModel 구현

---
+++ SimpleTableViewExample/SimpleTableViewExampleViewController/Numbers/NumbersViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  NumbersViewModel.swift
+//  SimpleTableViewExampleViewController
+//
+//  Created by Lee on 8/21/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class NumbersViewModel {
+
+    var disposeBag = DisposeBag()
+    // Input: textField 3개
+    // Output: 연산한 결과
+
+    struct Input {
+        var firstNumberTextField: ControlProperty<String>
+        var secondNumberTextField: ControlProperty<String>
+        var thirdNumberTextField: ControlProperty<String>
+    }
+
+    struct Output {
+        var calculateResult: BehaviorSubject<String> = BehaviorSubject(value: "")
+    }
+
+    func transform(input: Input) -> Output {
+
+        let calculateResult = BehaviorSubject(value: "")
+
+        Observable
+            .combineLatest(input.firstNumberTextField.asObservable(),
+                           input.secondNumberTextField.asObservable(),
+                           input.thirdNumberTextField.asObservable())
+            .map { first, second, third in
+                let firstNum = Int(first) ?? 0
+                let secondNum = Int(second) ?? 0
+                let thirdNum = Int(third) ?? 0
+                return String(firstNum + secondNum + thirdNum)
+            }
+            .bind(to: calculateResult)
+            .disposed(by: disposeBag)
+
+        return NumbersViewModel.Output(calculateResult: calculateResult)
+    }
+}
